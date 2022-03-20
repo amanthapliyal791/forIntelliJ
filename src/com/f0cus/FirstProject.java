623effc3fa83fbb0da6ffc9cbb0e272ceb7b5b1d
Aman Thapliyal <aman.thapliyal791@gmail.com>
2022-03-20 12:46:34 +0530

    SimpleClass parameterized

--- src/com/f0cus/FirstProject.java
+++ src/com/f0cus/FirstProject.java
@@ -2,6 +2,8 @@ package com.f0cus;
 
 import com.f0cus.interfaces.Contract;
 
+import java.util.List;
+
 public class FirstProject {
 
     public static void main(String[] args) {
@@ -9,9 +11,15 @@ public class FirstProject {
         SimpleClass obj = new SimpleClass();
 
 	    System.out.println(obj.getResult());
-
         Contract con = new SimpleClass();
+
         System.out.println(con.showCompleteContract());
         System.out.println("Bye!!");
+
+        List<String> names = List.of("a","b","c");
+
+        for (String name : names) {
+            System.out.println("name = " + name);
+        }
     }
 }
